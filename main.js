d49745c868585618ce13814c9765a48b81ff0cb4
shreyaagrawal <shreyaagrawal@users.noreply.github.com>
2021-01-31 23:13:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+var canvas = new.fabric.Canvas('mycanvas');
+
+player_x =10;
+player_y =10;
+
+block_image_width= 30;
+block_image_height= 30;
+
+var player_object="";
+var block_image_object="";
+
+function player_update(){
+    fabric.Image.fromURL("superhero.jpg", function(Img){
+        player_object =Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_x,
+            left:player_y
+        });
+        canvas.add(player_object);
+    });
+}
+
+function new_image(get_image){
+    fabric.Image.fromURL(get_image, function(Img){
+        block_image_object =Img;
+        block_image_object.scaleToWidth(150);
+        block_image_object.scaleToHeight(140);
+        block_image_object.set({
+            top:player_x,
+            left:player_y
+        });
+        canvas.add(block_image_object);
+    });
+}
+
